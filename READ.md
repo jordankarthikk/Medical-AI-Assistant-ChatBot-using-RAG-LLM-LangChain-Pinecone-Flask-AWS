bb1840d26faa602398058ec1d7d0a323424dd8c6
jordankarthikk <jordankarthikk@gmail.com>
2026-06-18 13:31:41 +0530

    Requirements added

---
+++ READ.md
@@ -0,0 +1,9 @@
+CLONE REPO -->
+
+git clone https://github.com/jordankarthikk/Medical-AI-Assistant-ChatBot-using-RAG-LLM-LangChain-Pinecone-Flask-AWS.git
+
+conda create -n MediBot python=3.12 -y
+conda activate MediBot
+
+
+python 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='medical_chatbot',
+    version='0.1.0',
+    author='karthikeyan',
+    author_email="jordankarthikk@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
